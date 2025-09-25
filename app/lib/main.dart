0f09f9848314e1a430bff1a6e2129e58be6d75f4
th33raphat <theeraphatchueanokkhum@gmail.com>
2025-09-25 13:33:15 +0700

    feat: setup storage service with gcloud

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:app/config/share/app_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -7,7 +9,12 @@ import 'pages/onboarding/onboarding.page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
 
   runApp(
     MultiProvider(
