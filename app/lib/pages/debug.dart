8c37275629cb172008845086c5a62e57ed83e81a
th33raphat <theeraphatchueanokkhum@gmail.com>
2025-09-26 19:26:38 +0700

    feat: fix register form state

--- app/lib/pages/debug.dart
+++ app/lib/pages/debug.dart
@@ -96,15 +96,16 @@ class DebugPage extends StatelessWidget {
             //     StepData(label: 'Step 4', active: true),
             //   ],
             // ),
-            // HeaderStepperCard(
-            //   steps: [
-            //     StepData(label: 'Step 1', active: true),
-            //     StepData(label: 'Step 2', active: true),
-            //     StepData(label: 'Step 3', active: true),
-            //     StepData(label: 'Step 4', active: false),
-            //   ],
-            // ),
-            LocationSelectorPage(),
+            HeaderStepperCard(
+              steps: [
+                StepData(label: 'Step 1', active: true),
+                StepData(label: 'Step 2', active: true),
+                StepData(label: 'Step 3', active: true),
+                StepData(label: 'Step 4', active: false),
+              ],
+            ),
+
+            // LocationSelectorPage(),
           ],
         ),
       ),
