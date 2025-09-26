6e2cbbb1493a1aea15890b9fa3492a204a030168
NKitsa <ninenazaza001@gmail.com>
2025-09-26 17:41:03 +0700

    finish_login_ui

--- app/lib/pages/debug.dart
+++ app/lib/pages/debug.dart
@@ -15,86 +15,86 @@ class DebugPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            // ButtonActions(text: 'Button', variant: ButtonVariant.primary),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.light),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.outline),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.warning),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.danger),
+            ButtonActions(text: 'Button', variant: ButtonVariant.primary),
+            ButtonActions(text: 'Button', variant: ButtonVariant.light),
+            ButtonActions(text: 'Button', variant: ButtonVariant.outline),
+            ButtonActions(text: 'Button', variant: ButtonVariant.warning),
+            ButtonActions(text: 'Button', variant: ButtonVariant.danger),
 
-            // ButtonActions(
-            //   text: 'Button',
-            //   variant: ButtonVariant.primary,
-            //   icon: Icons.add,
-            //   iconPosition: IconPosition.right,
-            // ),
-            // ButtonActions(
-            //   text: 'Button',
-            //   variant: ButtonVariant.primary,
-            //   icon: Icons.add,
-            //   iconPosition: IconPosition.left,
-            // ),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.light),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.outline),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.warning),
-            // ButtonActions(text: 'Button', variant: ButtonVariant.danger),
+            ButtonActions(
+              text: 'Button',
+              variant: ButtonVariant.primary,
+              icon: Icons.add,
+              iconPosition: IconPosition.right,
+            ),
+            ButtonActions(
+              text: 'Button',
+              variant: ButtonVariant.primary,
+              icon: Icons.add,
+              iconPosition: IconPosition.left,
+            ),
+            ButtonActions(text: 'Button', variant: ButtonVariant.light),
+            ButtonActions(text: 'Button', variant: ButtonVariant.outline),
+            ButtonActions(text: 'Button', variant: ButtonVariant.warning),
+            ButtonActions(text: 'Button', variant: ButtonVariant.danger),
 
-            // ButtonUnderline(text: 'Button'),
-            // ButtonUnderline(text: 'Button', active: true),
+            ButtonUnderline(text: 'Button'),
+            ButtonUnderline(text: 'Button', active: true),
 
-            // HeaderCard(),
+            HeaderCard(),
 
-            // InputField(
-            //   label: 'Input',
-            //   type: InputType.line,
-            //   hintText: 'Input',
-            //   validate: true,
-            //   errorText: 'Error',
-            // ),
-            // InputField(
-            //   label: 'Input',
-            //   type: InputType.fill,
-            //   hintText: 'Input',
-            //   validate: true,
-            //   errorText: 'Error',
-            // ),
-            // InputField(
-            //   label: 'Input',
-            //   type: InputType.fill,
-            //   hintText: 'Input',
-            //   validate: true,
-            //   errorText: 'Error',
-            //   suffixIcon: const Icon(Icons.add),
-            // ),
+            InputField(
+              label: 'Input',
+              type: InputType.line,
+              hintText: 'Input',
+              validate: true,
+              errorText: 'Error',
+            ),
+            InputField(
+              label: 'Input',
+              type: InputType.fill,
+              hintText: 'Input',
+              validate: true,
+              errorText: 'Error',
+            ),
+            InputField(
+              label: 'Input',
+              type: InputType.fill,
+              hintText: 'Input',
+              validate: true,
+              errorText: 'Error',
+              suffixIcon: const Icon(Icons.add),
+            ),
 
-            // ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xl),
-            // ProfileWidgets.avatar(isEdited: true, size: ProfileSize.md),
-            // ProfileWidgets.avatar(isEdited: true, size: ProfileSize.sm),
-            // ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xs),
-            // ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xxs),
-            // StepperWidget(
-            //   steps: [
-            //     StepData(label: 'Step 1', active: true),
-            //     StepData(label: 'Step 2', active: false),
-            //     StepData(label: 'Step 3', active: false),
-            //   ],
-            // ),
+            ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xl),
+            ProfileWidgets.avatar(isEdited: true, size: ProfileSize.md),
+            ProfileWidgets.avatar(isEdited: true, size: ProfileSize.sm),
+            ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xs),
+            ProfileWidgets.avatar(isEdited: true, size: ProfileSize.xxs),
+            StepperWidget(
+              steps: [
+                StepData(label: 'Step 1', active: true),
+                StepData(label: 'Step 2', active: false),
+                StepData(label: 'Step 3', active: false),
+              ],
+            ),
 
-            // StepperWidget(
-            //   steps: [
-            //     StepData(label: 'Step 1', active: true),
-            //     StepData(label: 'Step 2', active: true),
-            //     StepData(label: 'Step 3', active: true),
-            //   ],
-            // ),
+            StepperWidget(
+              steps: [
+                StepData(label: 'Step 1', active: true),
+                StepData(label: 'Step 2', active: true),
+                StepData(label: 'Step 3', active: true),
+              ],
+            ),
 
-            // StepperWidget(
-            //   steps: [
-            //     StepData(label: 'Step 1', active: true),
-            //     StepData(label: 'Step 2', active: true),
-            //     StepData(label: 'Step 3', active: true),
-            //     StepData(label: 'Step 4', active: true),
-            //   ],
-            // ),
+            StepperWidget(
+              steps: [
+                StepData(label: 'Step 1', active: true),
+                StepData(label: 'Step 2', active: true),
+                StepData(label: 'Step 3', active: true),
+                StepData(label: 'Step 4', active: true),
+              ],
+            ),
           ],
         ),
       ),
